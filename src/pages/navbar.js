373d94b92f8e467d71aacf4945ec2018c3c2c430
Vinit-Tiwari <vinitvtiwari0505@gmail.com>
2022-10-30 01:25:56 +0530

    second

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'; 
+import { Link } from "react-router-dom";
 import '../css/home.css';
 
 function Navbar(){
@@ -12,25 +13,33 @@ function Navbar(){
                     </span>
 
                     <span className="menu row">
+                        <Link to={'/'}>
                             <span className="row options selected-option">
                                 <p>00</p>
                                 <p>Home</p>
                             </span>
+                        </Link>
 
+                        <Link to={'destination'}>
                             <span className="row options">
                                 <p>01</p>
                                 <p>Destination</p>
                             </span>
+                        </Link>
 
+                        <Link to={'crew'}>
                             <span className="row options">
                                 <p>02</p>
                                 <p>Crew</p>
                             </span>
+                        </Link>
 
+                        <Link to={'technology'}>
                             <span className="row options">
                                 <p>03</p>
                                 <p>Technology</p>
                             </span>
+                        </Link>
                     </span>
                 </span>
             </span>
