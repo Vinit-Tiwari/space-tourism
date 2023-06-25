b1e3af3715db84c313e741d06f60c8559b7f97c8
Vinit-Tiwari <vinitvtiwari0505@gmail.com>
2023-06-25 20:30:35 +0530

    Completed project

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,9 @@
+import {useState} from "react";
 import styles from "../styles/navbar.module.css"
 import {useRouter} from "next/router";
 
 function Navbar() {
+  const [menuSelected,setMenuSelected]=useState(false)
   const router=useRouter()
   return (
     <span className={styles.container}>
@@ -16,15 +18,16 @@ function Navbar() {
           </g>
         </svg>
       </span>
-      <span className="hidden lg:flex h-[0.1rem] bg-white w-[35%] relative left-[8%] z-30 opacity-40 md:hidden"></span>
+      <span className="hidden lg:flex font-bold text-[16px] h-[0.1rem] bg-white w-[35%] relative left-[8%] z-30 opacity-40 md:hidden"></span>
       <span className={styles.menu}>
         <button
           type="button"
           onClick={() => {
             router.push("/");
           }}
+          className={router.route==="/"? "border-b-4":""}
         >
-          <p className="hidden lg:flex">00</p>
+          <p className="hidden lg:flex font-bold text-[16px]">00</p>
           HOME
         </button>
 
@@ -33,8 +36,9 @@ function Navbar() {
           onClick={() => {
             router.push("/destination/moon");
           }}
+          className={router.route==="/destination/[id]"? "border-b-4":""}
         >
-          <p className="hidden lg:flex">01</p>
+          <p className="hidden lg:flex font-bold text-[16px]">01</p>
           DESTINATION
         </button>
 
@@ -43,8 +47,9 @@ function Navbar() {
           onClick={() => {
             router.push("/crew/commander");
           }}
+          className={router.route==="/crew/[id]"? "border-b-4":""}
         >
-          <p className="hidden lg:flex">02</p>
+          <p className="hidden lg:flex font-bold text-[16px]">02</p>
           CREW
         </button>
 
@@ -53,11 +58,71 @@ function Navbar() {
           onClick={() => {
             router.push("/technology/vehicle");
           }}
+          className={router.route==="/technology/[id]"? "border-b-4":""}
         >
-          <p className="hidden lg:flex">03</p>
+          <p className="hidden lg:flex font-bold text-[16px]">03</p>
           TECHNOLOGY
         </button>
       </span>
+
+      <span onClick={()=> setMenuSelected(!menuSelected)} className="flex md:hidden lg:hidden text-white flex flex-col gap-[2rem] z-[10] items-center justify-center">
+        {menuSelected? <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="Group"><rect id="Rectangle Copy" x="2.5752" y="0.954102" width="24" height="3" transform="rotate(45 2.5752 0.954102)" fill="#D0D6F9" /><rect id="Rectangle Copy 3" x="0.454102" y="17.9246" width="24" height="3" transform="rotate(-45 0.454102 17.9246)" fill="#D0D6F9" /></g></svg>:
+          <svg width="24" height="21" viewBox="0 0 24 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="24" height="3" fill="#D0D6F9" />
+            <rect y="9" width="24" height="3" fill="#D0D6F9" />
+            <rect y="18" width="24" height="3" fill="#D0D6F9" />
+          </svg>
+        }
+      </span>
+
+      {menuSelected?
+      <span className={styles.menuoptions}>
+          <button
+            type="button"
+            onClick={() => {
+              router.push("/");
+            }}
+            className={router.route==="/"? "border-b-4":""}
+          >
+            <p className="lg:flex font-bold text-[16px]">00</p>
+            HOME
+          </button>
+
+          <button
+            type="button"
+            onClick={() => {
+              router.push("/destination/moon");
+            }}
+            className={router.route==="/destination/[id]"? "border-b-4":""}
+          >
+            <p className="lg:flex font-bold text-[16px]">01</p>
+            DESTINATION
+          </button>
+
+          <button
+            type="button"
+            onClick={() => {
+              router.push("/crew/commander");
+            }}
+            className={router.route==="/crew/[id]"? "border-b-4":""}
+          >
+            <p className="lg:flex font-bold text-[16px]">02</p>
+            CREW
+          </button>
+
+          <button
+            type="button"
+            onClick={() => {
+              router.push("/technology/vehicle");
+            }}
+            className={router.route==="/technology/[id]"? "border-b-4":""}
+          >
+            <p className="lg:flex font-bold text-[16px]">03</p>
+            TECHNOLOGY
+          </button>
+      </span>
+      :false}
+
     </span>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ export default function Home() {
     <Layout>
       <span className={style.bg}>
         <span className="max-w-[65rem] w-full flex flex-col">
-          <span className={style.subheading}>SO, YOU WANT TO TRAVEL TO</span>
+          <span className="text-center lg:text-left text-[#D0D6F9] text-[1.2rem] w-full opacity-80">SO, YOU WANT TO TRAVEL TO</span>
 
           <span className="flex items-center justify-between gap-5 md:flex-col flex-col lg:flex-row">
             <span className="flex flex-col gap-10">
-              <p className={style.title}>SPACE</p>
+              <p className="text-[80px] md:text-[150px] lg:text-[150px] flex items-center justify-center h-[10rem]">SPACE</p>
 
-              <p className="max-w-[25rem] opacity-60">
+              <p className="text-[16px] text-center lg:text-left max-w-[25rem] text-[#D0D6F9]">
                 Let’s face it; if you want to go to space, you might as well
                 genuinely go to outer space and not hover kind of on the edge of
                 it. Well sit back, and relax because we’ll give you a truly out
